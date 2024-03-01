85a131d0f0894421cd4a3a5401e388bcd941cab2
Rodrigo Yokota <ryok90@gmail.com>
2024-02-29 22:35:19 -0300

    feat: using different chakra versions between remote and host

--- app2/src/components/App.js
+++ app2/src/components/App.js
@@ -1,8 +1,9 @@
-import LocalButton from "./Button";
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import LocalButton from './Button';
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import pkg from '@chakra-ui/react/package.json';
 
-const Wrapper = (children) => {
+const Wrapper = ({ children }) => {
   return <ChakraProvider>{children}</ChakraProvider>;
 };
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <div>
         <h1>Basic Host-Remote</h1>
         <h2>App 2</h2>
+        <p>Chakra Version from Remote: {pkg.version}</p>
         <LocalButton />
       </div>
     </Wrapper>
